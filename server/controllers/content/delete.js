2bd13101981960e5f7a01b671b7b6ac9e0dac9b1
minjman2659 <minjman2659@gmail.com>
2021-09-11 17:37:47 +0900

    [Server] controllers_content : My Page 및 thumbsup API 구현

--- server/controllers/content/delete.js
+++ server/controllers/content/delete.js
@@ -1,5 +1,73 @@
+const { content, user_contents, word } = require('../../models');
+const {
+  generateAccessToken,
+  isAuthorized,
+} = require('../tokenFunction/accessToken');
+const { refreshAuthorized } = require('../tokenFunction/refreshToken');
+
 module.exports = {
-  post: (req, res) => {
-    res.send('This is content/delete');
+  post: async (req, res) => {
+    const accessTokenCheck = isAuthorized(req);
+    const refreshTokenCheck = refreshAuthorized(req);
+
+    if (accessTokenCheck) {
+      // accessToken이 만료되지 않았을 경우,
+      // => 바로 요청에 대한 응답 제공
+      const { contentId } = req.body;
+      await content.destroy({
+        where: { id: contentId },
+        force: true,
+      });
+      await user_contents.destroy({
+        where: { content_Id: contentId },
+        force: true,
+      });
+      const allMyContents = await content.findAll({
+        where: { userId: accessTokenCheck.id },
+      });
+      const returnData = allMyContents.map(async (el) => {
+        el.wordName = await word.findOne({
+          where: { id: el.wordId },
+        }).wordName;
+        return el;
+      });
+      res.status(200).json({ data: returnData });
+    } else {
+      // accessToken이 만료되어서 refreshToken을 판별하고,
+      // refreshToken은 만료되지 않았을 경우,
+      // => 요청에 대한 응답과 함께 새로 만든 accessToken 발급
+      if (refreshTokenCheck) {
+        delete refreshTokenCheck.exp;
+        const accessToken = generateAccessToken(refreshTokenCheck);
+
+        const { contentId } = req.body;
+        await content.destroy({
+          where: { id: contentId },
+          force: true,
+        });
+        await user_contents.destroy({
+          where: { content_Id: contentId },
+          force: true,
+        });
+        const allMyContents = await content.findAll({
+          where: { userId: refreshTokenCheck.id },
+        });
+        const returnData = allMyContents.map(async (el) => {
+          el.wordName = await word.findOne({
+            where: { id: el.wordId },
+          }).wordName;
+          return el;
+        });
+        res.status(201).json({
+          accessToken: accessToken,
+          data: returnData,
+        });
+      } else {
+        // accessToken이 만료되어서 refreshToken을 판별하고,
+        // refreshToken도 만료되었을 경우,
+        // 클라이언트에게 다시 로그인을 하라는 메시지 응답을 보낸다.
+        res.status(401).json({ message: 'Send new Login Request' });
+      }
+    }
   },
 };
