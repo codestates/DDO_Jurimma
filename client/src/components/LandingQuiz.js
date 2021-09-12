045520db14471215da1b65ce5e2849623938c0ee
Youngseo Kang <youngseo.kangg@gmail.com>
2021-09-12 17:55:15 +0900

    :sparkles: [Client] LandingQuiz : 랜딩페이지 퀴즈 제작

--- client/src/components/LandingQuiz.js
+++ client/src/components/LandingQuiz.js
@@ -1,14 +1,200 @@
 // 랜딩페이지에서 맛보기로 제공될 퀴즈
 import styled from 'styled-components';
+import { useState } from 'react';
+import you_quiz from '../images/you_quiz.svg';
 
-const LandingQuizWrap = styled.div`
-  width: 100%; // 콘텐츠 전체 길이 생각해서 후에 수정해주기
-  border: 1px solid red;
+const LandingQuizBox = styled.article`
+  width: max(340px, 50vw);
+  max-width: 500px;
+  height: max(420px, 50vh);
+  margin: auto;
+  border-radius: 15px;
+  padding: 20px;
   box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #b4aee8;
+`;
+
+const LandingQuizScore = styled.div`
+  align-items: center;
+  text-align: center;
+  word-break: break-all;
+  width: 100%;
+  > img {
+    display: block;
+    width: 150px;
+    height: 150px;
+    margin: 0 auto 20px;
+  }
+  > div {
+    height: 200px;
+    background-color: #fff;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    > p:nth-child(1) {
+      font-size: 1.6rem;
+      margin-bottom: 30px;
+    }
+    > p:nth-child(2) {
+      font-size: 1rem;
+    }
+  }
+`;
+
+const LandingQuizQuestion = styled.div`
+  margin-bottom: 20px;
+  width: 100%;
+  color: #fff;
+  > img {
+    display: block;
+    width: 300px;
+    height: 300px;
+    margin: auto;
+  }
+`;
+
+const LandingQuizChoices = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const LandingQuizCount = styled.div`
+  margin-bottom: 20px;
+  > span {
+    font-size: 28px;
+  }
+`;
+
+const LandingQuizText = styled.div`
+  width: 100%;
+  font-size: 2rem;
+`;
+
+const LandingQuizChoiceButton = styled.button`
+  width: 100%;
+  height: 70px;
+  font-size: 16px;
+  background-color: #fff;
+  border-radius: 80px;
+  display: flex;
+  padding: 5px;
+  margin-bottom: 10px;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  transition: all 0.3s;
+  :hover {
+    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  }
 `;
 
 function LandingQuiz() {
-  return <LandingQuizWrap>this is LandingQuiz</LandingQuizWrap>;
+  const questions = [
+    {
+      questionText: '',
+      answerOptions: [{ answerText: '시작하기', isCorrect: true }],
+    },
+    {
+      questionText: '자만추의 뜻으로 올바른 것은?',
+      answerOptions: [
+        { answerText: '자신만만 추성훈', isCorrect: false },
+        { answerText: '자연스럽게 만두 추가하고 포장', isCorrect: false },
+        { answerText: '자연스러운 만남 추구', isCorrect: true },
+      ],
+    },
+    {
+      questionText: '오저치고의 뜻으로 올바른 것은?',
+      answerOptions: [
+        { answerText: '오 저녀석 치고는 많이 고쳐서 냈다', isCorrect: false },
+        { answerText: '오늘 저녁 치킨 고?', isCorrect: true },
+        { answerText: '오금이 저리고 치통으로 고통받는다', isCorrect: false },
+      ],
+    },
+    {
+      questionText: '갑통알의 뜻으로 올바른 것은?',
+      answerOptions: [
+        { answerText: '갑자기 통장을 보니 알바를 해야겠다', isCorrect: true },
+        { answerText: '갑자기 통증을 알아버렸다', isCorrect: false },
+        {
+          answerText: '갑자기 통통튀는 알림 소리가 울렸다',
+          isCorrect: false,
+        },
+      ],
+    },
+  ]; // 문제
+
+  const [currentQuestion, setCurrentQuestion] = useState(0); // 현재 문제 index
+  const [showScore, setShowScore] = useState(false); // 점수 화면 보임 여부
+  const [score, setScore] = useState(-1); // 점수 카운트
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1); // 맞다면 점수에 ++1
+    }
+
+    const nextQuestion = currentQuestion + 1; // currentQuestion에 다음문제가 있나?
+    if (nextQuestion < questions.length) {
+      // 다음문제가 있다면
+      setCurrentQuestion(nextQuestion); // currentQuestion 정식으로 업데이트
+    } else {
+      // 다음문제가 없다면
+      setShowScore(true); // 점수 보여주기
+    }
+  };
+
+  return (
+    <LandingQuizBox>
+      {showScore ? ( // 점수 화면 보임 여부가 true라면
+        <LandingQuizScore>
+          <img src={you_quiz} />
+          <div>
+            <p>총 {score}개를 맞추셨습니다!</p>
+            <p>오늘의 퀴즈는 로그인 후 이용 가능합니다.</p>
+          </div>
+          {/*점수 보여줌 */}
+        </LandingQuizScore>
+      ) : (
+        // 점수 화면 보임 여부가 false 라면(=아직 풀 문제가 남음)
+        <>
+          <LandingQuizQuestion>
+            {score === -1 ? (
+              <img src={you_quiz} />
+            ) : (
+              <LandingQuizCount>
+                <span>Question {currentQuestion}</span>/{questions.length - 1}
+              </LandingQuizCount>
+            )}
+            <LandingQuizText>
+              {questions[currentQuestion].questionText}
+            </LandingQuizText>
+          </LandingQuizQuestion>
+
+          {/* 선택지 */}
+          <LandingQuizChoices>
+            {questions[currentQuestion].answerOptions.map(
+              (answerOption, idx) => (
+                <LandingQuizChoiceButton
+                  key={idx}
+                  onClick={() =>
+                    handleAnswerOptionClick(answerOption.isCorrect)
+                  }
+                >
+                  {answerOption.answerText}
+                </LandingQuizChoiceButton>
+              )
+            )}
+          </LandingQuizChoices>
+        </>
+      )}
+    </LandingQuizBox>
+  );
 }
 
 export default LandingQuiz;
