e25b8e69c3660c941c086dbdf0a3716eb249d4e4
minjman2659 <minjman2659@gmail.com>
2021-09-13 19:48:06 +0900

    [Server] user 폴더 : isLogin 관련 API 수정

--- server/controllers/user/checkEmail.js
+++ server/controllers/user/checkEmail.js
@@ -5,7 +5,7 @@ module.exports = {
     const { email } = req.body;
 
     const emailCheck = await user.findOne({ where: { email: email } });
-    console.log('emailcheck : ', emailCheck);
+    // console.log('emailcheck : ', emailCheck);
     // db에 저장된 user email 정보가 겹치는지 확인해야한다
     // user 테이블의 email들 중 하나라도 겹치면 409 error
     if (emailCheck) {
