fe0760ed3043b078fbb564da89a7a8beff438eeb
Lee Naeun <nanni4287@gmail.com>
2021-09-13 13:36:57 +0900

    [Client] Navbar.js : nav bar scroll 효과 적용

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -1,9 +1,11 @@
 // Navbar 부분
-import styled from 'styled-components';
-import logo from '../images/main_logo.svg';
+import styled, { keyframes } from 'styled-components';
+import mainLogo from '../images/main_logo.svg';
+import whiteLogo from '../images/main_logoWhite.svg';
 import '../App.css';
 
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   setLoginOrSignupModal,
@@ -11,22 +13,32 @@ import {
   setLogoutModal,
 } from '../actions/index';
 
-const NavBar = styled.nav`
+const fadeIn = keyframes`
+  0%{opacity : 0}
+  100%{opacity : 1}
+`;
+
+const fadeout = keyframes`
+  0%{opacity : 0}
+  100%{opacity : 1}
+`;
+
+const NavBar1 = styled.nav`
   width: 100%;
-  height: 80px;
+  height: 85px;
   box-sizing: border-box;
   display: flex;
   position: fixed;
-  background-color: white;
+  animation: ${fadeout} 0.5s;
   > #logo {
     flex: 1 1 auto;
+    margin-top: 5px;
     > a {
-      > img {
-        display: block;
-        width: 80px;
-        height: 80px;
-        margin-left: 20px;
-      }
+      display: block;
+      width: 80px;
+      height: 80px;
+      margin-left: 20px;
+      background: url(${whiteLogo});
     }
   }
   > #menu_container {
@@ -42,7 +54,65 @@ const NavBar = styled.nav`
       line-height: 50px;
       text-align: center;
       margin-left: max(1vw, 10px);
-      font-size: max(1.2vw, 16px);
+      font-size: max(1.2vw, 18px);
+      font-family: 'NEXON Lv2 Gothic Bold';
+      color: #fff; // login, quiz 부분
+      transition: 0.5s;
+      > a {
+        width: max(8vw, 100px);
+        height: 50px;
+        display: block;
+        border-radius: 50px;
+        text-decoration-line: none;
+        color: #fff; // main 부분
+      }
+      > a:hover {
+        color: #440a67;
+      }
+    }
+    > .menu:hover {
+      background-color: #fff;
+      color: #440a67;
+      cursor: pointer;
+    }
+  }
+`;
+
+const NavBar2 = styled.nav`
+  width: 100%;
+  height: 85px;
+  box-sizing: border-box;
+  display: flex;
+  position: fixed;
+  background-color: #fff;
+  transition: 0.3s;
+  z-index: 9999;
+  animation: ${fadeIn} 0.8s;
+  > #logo {
+    flex: 1 1 auto;
+    margin-top: 5px;
+    > a {
+      display: block;
+      width: 80px;
+      height: 80px;
+      margin-left: 20px;
+      background: url(${mainLogo});
+    }
+  }
+  > #menu_container {
+    display: flex;
+    flex: 6 1 auto;
+    justify-content: flex-end;
+    margin-right: 20px;
+    align-items: center;
+    > .menu {
+      width: max(8vw, 100px);
+      height: 50px;
+      border-radius: 50px;
+      line-height: 50px;
+      text-align: center;
+      margin-left: max(1vw, 10px);
+      font-size: max(1.2vw, 18px);
       font-family: 'NEXON Lv2 Gothic Bold';
       color: #440a67; // login, quiz 부분
       transition: 0.5s;
@@ -70,6 +140,7 @@ function Nav() {
   const state = useSelector((state) => state.userInfoReducer);
   const nowDate = new Date().toLocaleDateString(); // 접속한 날짜를 "2021. 9. 12."와 같은 형식으로 확인
   const dispatch = useDispatch();
+  const [navBarScroll, setNavBarScroll] = useState(false); // nav bar 스크롤 했을 때
 
   const openQuizModal = (isOpen) => {
     if (nowDate !== state.lastQuiz && state.isLogin) {
@@ -90,34 +161,78 @@ function Nav() {
     dispatch(setLogoutModal(isOpen));
   }; // 로그아웃 모달 여는 함수
 
+  const scrollNavChange = () => {
+    if (window.scrollY >= 200) {
+      setNavBarScroll(true);
+    } else {
+      setNavBarScroll(false);
+    }
+  }; // 지정된 스크롤에 닿았을 때 nav bar 변경
+
+  window.addEventListener('scroll', scrollNavChange);
+
   return (
-    <NavBar>
-      <div id='logo' className='navInner'>
-        <Link to='/'>
-          <img src={logo} alt='Jurimma_logo' />
-        </Link>
-      </div>
-      <div id='menu_container' className='navInner'>
-        <div className='menu'>
-          <Link to='/main'>Main</Link>
-        </div>
-        <div className='menu' onClick={() => openQuizModal(true)}>
-          Quiz
-        </div>
-        {state.isLogin ? (
-          <div>
-            <Link to='/mypage'>mypage</Link>
-            <div className='menu' onClick={() => openLogoutModal(true)}>
-              Logout
+    <>
+      {navBarScroll ? (
+        <NavBar2>
+          <div id='logo' className='navInner'>
+            <Link to='/'></Link>
+          </div>
+          <div id='menu_container' className='navInner'>
+            <div className='menu'>
+              <Link to='/main'>Main</Link>
             </div>
+            <div className='menu' onClick={() => openQuizModal(true)}>
+              Quiz
+            </div>
+            {state.isLogin ? (
+              <div>
+                <Link to='/mypage'>mypage</Link>
+                <div className='menu' onClick={() => openLogoutModal(true)}>
+                  Logout
+                </div>
+              </div>
+            ) : (
+              <div
+                className='menu'
+                onClick={() => openLoginOrSignupModal(true)}
+              >
+                Login
+              </div>
+            )}
           </div>
-        ) : (
-          <div className='menu' onClick={() => openLoginOrSignupModal(true)}>
-            Login
+        </NavBar2>
+      ) : (
+        <NavBar1>
+          <div id='logo' className='navInner'>
+            <Link to='/'></Link>
           </div>
-        )}
-      </div>
-    </NavBar>
+          <div id='menu_container' className='navInner'>
+            <div className='menu'>
+              <Link to='/main'>Main</Link>
+            </div>
+            <div className='menu' onClick={() => openQuizModal(true)}>
+              Quiz
+            </div>
+            {state.isLogin ? (
+              <div>
+                <Link to='/mypage'>mypage</Link>
+                <div className='menu' onClick={() => openLogoutModal(true)}>
+                  Logout
+                </div>
+              </div>
+            ) : (
+              <div
+                className='menu'
+                onClick={() => openLoginOrSignupModal(true)}
+              >
+                Login
+              </div>
+            )}
+          </div>
+        </NavBar1>
+      )}
+    </>
   );
 }
 
