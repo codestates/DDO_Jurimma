85519bc56345881e3668ace273d48318d28451e8
minjman2659 <minjman2659@gmail.com>
2021-09-13 13:42:07 +0900

    [Server] controllers : delete, edition, search API 수정

--- server/controllers/content/delete.js
+++ server/controllers/content/delete.js
@@ -25,13 +25,17 @@ module.exports = {
       const allMyContents = await content.findAll({
         where: { userId: accessTokenCheck.id },
       });
-      const returnData = allMyContents.map(async (el) => {
-        el.dataValues.wordName = await word.findOne({
+      // console.log('allMyContents 1 : ', allMyContents);
+      for (let el of allMyContents) {
+        // console.log('el.wordId : ', el.wordId);
+        const matchingWord = await word.findOne({
           where: { id: el.wordId },
-        }).wordName;
-        return el;
-      });
-      res.status(200).json({ data: returnData });
+        });
+        // console.log('matchingWord : ', matchingWord.dataValues.wordName);
+        el.dataValues.wordName = matchingWord.dataValues.wordName;
+      }
+      // console.log('allMyContents 2 : ', allMyContents);
+      res.status(200).json({ data: allMyContents });
     } else {
       // accessToken이 만료되어서 refreshToken을 판별하고,
       // refreshToken은 만료되지 않았을 경우,
@@ -52,15 +56,19 @@ module.exports = {
         const allMyContents = await content.findAll({
           where: { userId: refreshTokenCheck.id },
         });
-        const returnData = allMyContents.map(async (el) => {
-          el.dataValues.wordName = await word.findOne({
+        // console.log('allMyContents 1 : ', allMyContents);
+        for (let el of allMyContents) {
+          // console.log('el.wordId : ', el.wordId);
+          const matchingWord = await word.findOne({
             where: { id: el.wordId },
-          }).wordName;
-          return el;
-        });
+          });
+          // console.log('matchingWord : ', matchingWord.dataValues.wordName);
+          el.dataValues.wordName = matchingWord.dataValues.wordName;
+        }
+        // console.log('allMyContents 2 : ', allMyContents);
         res.status(201).json({
           accessToken: accessToken,
-          data: returnData,
+          data: allMyContents,
         });
       } else {
         // accessToken이 만료되어서 refreshToken을 판별하고,
