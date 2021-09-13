eb6ccae606a3ceb88a7b83211199ded849af30c1
mniYUNSU <ownsgks@gmail.com>
2021-09-13 14:51:50 +0900

    [Server] login.js - 중복 로그인 방지 로직 구현

--- server/controllers/user/login.js
+++ server/controllers/user/login.js
@@ -20,12 +20,18 @@ module.exports = {
       if (decryptedPw !== password) {
         res.status(400).json({ message: 'Invalid User' });
       }
+      // 중복 로그인인 경우
+      else if (userInfo.isLogin === true) {
+        res.status(409).json({ message: 'Already Logged In' });
+      }
       // 그 외의 경우는 로그인 성공, accessToken과 userInfo를 return
       else {
         delete userInfo.dataValues.password;
         const accessToken = generateAccessToken(userInfo.dataValues);
         const refreshToken = generateRefreshToken(userInfo.dataValues);
         sendRefreshToken(res, refreshToken);
+        userInfo.isLogin = true;
+        await userInfo.save();
         res.status(200).json({ accessToken, userInfo });
       }
     }
--- server/controllers/user/logout.js
+++ server/controllers/user/logout.js
@@ -1,3 +1,4 @@
+const { user } = require('../../models');
 const { isAuthorized } = require('../tokenFunction/accessToken');
 const { sendRefreshToken } = require('../tokenFunction/refreshToken');
 
@@ -10,6 +11,12 @@ module.exports = {
     // 그 외의 경우 accessToken, refreshToken 삭제
     else {
       sendRefreshToken(res, null);
+      const userInfo = await user.findOne({
+        where: { email: isAuthorized(req).email },
+      });
+      // 로그아웃할 때 유저의 로그인 상태 변경
+      userInfo.isLogin = false;
+      await userInfo.save();
       res.status(200).json({ accessToken: null, message: 'ok' });
     }
   },
