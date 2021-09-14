0cb95186e988aab2f4e60eebc0212bbbd2d2fc69
Lee Naeun <nanni4287@gmail.com>
2021-09-14 21:11:52 +0900

    [client] LoginOrSignUp.js : position fixed 수정

--- client/src/modals/LoginOrSignUp.js
+++ client/src/modals/LoginOrSignUp.js
@@ -12,8 +12,12 @@ import swal from 'sweetalert';
 library.add(fab, faComment);
 
 const LoginOrSignupBackdrop = styled.div`
-  width: 100vw;
-  height: 100vh;
+  /* width: 100vw;
+  height: 100vh; */
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
   background-color: rgba(0, 0, 0, 0.7);
   display: flex;
   justify-content: center;
