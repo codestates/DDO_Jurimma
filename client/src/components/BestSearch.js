0eae2f937bd6b447947bfcbf03b52f75c3c8b021
Lee Naeun <nanni4287@gmail.com>
2021-09-14 20:37:29 +0900

    [client] Main.js : main page 반응형 구조 변경

--- client/src/components/BestSearch.js
+++ client/src/components/BestSearch.js
@@ -2,7 +2,7 @@
 import styled from 'styled-components';
 
 const BestSearchWrap = styled.div`
-  height: 100%; // 콘텐츠 전체 길이 생각해서 후에 max 설정해주기
+  flex: 5 1 auto; // 콘텐츠 전체 길이 생각해서 후에 max 설정해주기
   border: 1px solid red;
   box-sizing: border-box;
 `;
