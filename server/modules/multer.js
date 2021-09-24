dcd27e4c77afd542b64b56ac8fa4cf18057a3992
mniYUNSU <ownsgks@gmail.com>
2021-09-25 00:55:19 +0900

    gitignore 재적용

--- server/modules/multer.js
+++ server/modules/multer.js
@@ -1,7 +1,15 @@
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const aws = require('aws-sdk');
-aws.config.loadFromPath(__dirname + '/../config/s3.json');
+require('dotenv').config();
+
+// aws.config.loadFromPath(__dirname + '/../config/s3.json');
+
+aws.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_ACCESS_SECRET_KEY,
+  region: 'ap-northeast-2',
+});
 
 const s3 = new aws.S3();
 const upload = multer({
