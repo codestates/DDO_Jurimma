5383bc172311cbd9becb993c7cd82040066dd145
Lee Naeun <nanni4287@gmail.com>
2021-09-27 17:43:30 +0900

    [Client] Footer.js : 색상변경

--- client/src/pages/Footer.js
+++ client/src/pages/Footer.js
@@ -1,16 +1,26 @@
 // footer 부분
 import styled from 'styled-components';
 
-const FooterBar = styled.footer`
+const FooterBarWrap = styled.footer`
   width: 100%;
-  height: max(133.39px, 20vh);
-  border: 1px solid red;
+  height: 200px;
+  background-color: #000;
   box-sizing: border-box;
   margin-top: 150px;
 `;
 
+const FooterBar = styled.div`
+  width: 90%;
+  height: 200px;
+  margin: 0 auto;
+`;
+
 function Footer() {
-  return <FooterBar>this is footer</FooterBar>;
+  return (
+    <FooterBarWrap>
+      <FooterBar></FooterBar>
+    </FooterBarWrap>
+  );
 }
 
 export default Footer;
