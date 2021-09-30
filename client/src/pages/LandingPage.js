46eaaceffb104a1d7372cf9824031ec4609eb021
Lee Naeun <nanni4287@gmail.com>
2021-09-30 22:27:14 +0900

    [Client] LandingPage.js : 웰컴 퀴즈 버튼 클릭시 이동 및 메인페이지 음성인식 추가

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import LandingSec1 from '../components/LandingSec1';
 import LandingSec2 from '../components/LandingSec2';
 import LandingQuiz from '../components/LandingQuiz';
+import { useRef } from 'react';
 
 const LandingPageWrap = styled.div`
   height: 100%; // 콘텐츠 전체 길이 생각해서 후에 max 설정해주기
@@ -10,13 +11,20 @@ const LandingPageWrap = styled.div`
 `;
 
 function LandingPage() {
-  let location = document.querySelector('quizSec');
+  const scrollFocus = useRef();
+  const scrollToQuiz = () => {
+    scrollFocus.current.scrollIntoView({
+      block: 'end',
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <>
       <LandingPageWrap>
         <LandingSec1 />
-        <LandingSec2 location={location} />
-        <LandingQuiz id='quizSec' />
+        <LandingSec2 scrollToQuiz={scrollToQuiz} />
+        <LandingQuiz scrollFocus={scrollFocus} />
       </LandingPageWrap>
     </>
   );
